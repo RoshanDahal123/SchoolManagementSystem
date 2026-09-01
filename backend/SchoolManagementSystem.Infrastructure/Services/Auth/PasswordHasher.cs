9ecaf002246394a19aa348724c9d1a7318de5005
RoshanDahal123 <dahalroshan765@gmail.com>
2026-09-01 23:34:49 +0545

    feat: implement pashword hashed in infrastructure

---
+++ backend/SchoolManagementSystem.Infrastructure/Services/Auth/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using BCrypt.Net;
+using SchoolManagementSystem.Application.Interfaces;
+
+
+namespace SchoolManagementSystem.Infrastructure.Services.Auth;
+
+public class PasswordHasher : IPasswordHasher
+{
+    private const int workFactor = 12;
+    public string Hash(string plainPassword)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(plainPassword, workFactor);
+    }
+
+    public bool Verify(string plainPassword, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(plainPassword, hash);
+    }
+}
+   
+
