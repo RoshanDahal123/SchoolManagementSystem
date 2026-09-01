6fbbf9713efd4937be995e7bd2971ac8927eb440
RoshanDahal123 <dahalroshan765@gmail.com>
2026-09-01 15:24:36 +0545

    feat: configure AppDbContext and EF Core mapping for User entity

---
+++ backend/SchoolManagementSystem.Infrastructure/SqlRepo/Persistence/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Domain.Entities;
+namespace SchoolManagementSystem.Infrastructure.SqlRepo.Persistence
+{
+   public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            // Picks up every IEntityTypeConfiguration < T > in this assembly.
+           // As Student, Teacher, etc. get their own configuration classes,
+           // they're automatically applied without touching this method again.
+           modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ backend/SchoolManagementSystem.Infrastructure/SqlRepo/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolManagementSystem.Domain.Entities;
+namespace SchoolManagementSystem.Infrastructure.SqlRepo.Persistence.Configurations;
+
+public class UserConfiguration: IEntityTypeConfiguration<User>
+{//fluent api mapping- maxlengths, uniqueIndex, enum conversion
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+        builder.HasKey(u=>u.Id);
+        builder.Property(u => u.FirstName).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(256);
+        builder.HasIndex(u => u.Email)
+            .IsUnique();
+        builder.Property(u => u.Role)
+           .IsRequired()
+           .HasConversion<string>()
+           .HasMaxLength(50);
+
+        builder.Property(u => u.IsActive)
+            .IsRequired();
+        builder.Property(u => u.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder.Property(u => u.CreatedAtUtc)
+            .IsRequired();
+
+        builder.Property(u => u.UpdatedAtUtc)
+            .IsRequired(false);
+    }
+}
