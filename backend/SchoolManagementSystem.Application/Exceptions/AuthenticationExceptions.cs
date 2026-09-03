d01180e3f9865fca772322928c6e7f6843e32b64
RoshanDahal123 <dahalroshan765@gmail.com>
2026-09-03 10:11:29 +0545

    feat:add AuthenticationExceptions

---
+++ backend/SchoolManagementSystem.Application/Exceptions/AuthenticationExceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagementSystem.Application.Exceptions
+{
+    public sealed class AuthenticationException:Exception
+    {
+          public AuthenticationException(string message) : base(message) { }
+    }
+}
