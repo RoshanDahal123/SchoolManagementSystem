1a96c4f4e0bdad6ce07626923999af56cfaf855e
RoshanDahal123 <dahalroshan765@gmail.com>
2026-09-03 10:10:53 +0545

    feat:DTOs form response and login request

---
+++ backend/SchoolManagementSystem.Application/DTOs/Auth/AuthResponse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SchoolManagementSystem.Application.DTOs.Auth;
+
+public sealed record AuthResponse(
+    string AccessToken,
+    DateTime ExpiresAtUtc,
+    string Email,
+    string FirstName,
+    string LastName,
+    string Role);
