a11ded49ac59982b0b893a209e11d5afeb7d3339
RoshanDahal123 <dahalroshan765@gmail.com>
2026-09-04 14:29:17 +0545

    feat: implement AccountSetupTokenRepository

---
+++ backend/SchoolManagementSystem.Application/Interfaces/IAccountSetupTokenRepository.cs
@@ -0,0 +1,15 @@
+﻿using SchoolManagementSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagementSystem.Application.Interfaces
+{
+    public interface IAccountSetupTokenRepository
+    {
+        Task AddAsync(AccountSetupToken token, CancellationToken ct = default);
+        Task<AccountSetupToken?> GetByTokenHashAsync(string tokenHash, CancellationToken ct = default);
+
+        Task SaveChangesAsync(CancellationToken ct = default);
+    }
+}
