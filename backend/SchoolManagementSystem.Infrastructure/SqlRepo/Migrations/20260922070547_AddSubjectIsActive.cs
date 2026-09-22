8c96380fc7077f07494099e226c77012e4a002df
RoshanDahal123 <dahalroshan765@gmail.com>
2026-09-22 15:35:41 +0545

    added migrations to add the subject isActive or not.

---
+++ backend/SchoolManagementSystem.Infrastructure/SqlRepo/Migrations/20260922070547_AddSubjectIsActive.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagementSystem.Infrastructure.SqlRepo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubjectIsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_Code",
+                table: "Subjects");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Subjects",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_Code",
+                table: "Subjects",
+                column: "Code",
+                unique: true,
+                filter: "[IsActive]=1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_Code",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Subjects");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_Code",
+                table: "Subjects",
+                column: "Code",
+                unique: true);
+        }
+    }
+}
